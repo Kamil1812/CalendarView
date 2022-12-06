927346b502069ff81bb7255d96f1a1a8205f2378
Majster na beacie ziomal, latam na dronie jak na tureckim dywanie, pytasz czy jaram? <majster247.bezdowozu@kxh-research.gov>
2022-12-06 09:31:43 +0100

    Ustawienie aktualnej daty w CalendarView

--- app/src/main/java/com/example/calendarview/MainActivity.kt
+++ app/src/main/java/com/example/calendarview/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.calendarview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CalendarView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.Calendar
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val cal = CalendarView(this)
+        cal.setDate(Calendar.getInstance().getTimeInMillis(),false,true);
     }
 }
