828943e9e5a858cdd1ab3f9bb4a1049c4d7acae5
Majster na beacie ziomal, latam na dronie jak na tureckim dywanie, pytasz czy jaram? <majster247.bezdowozu@kxh-research.gov>
2022-12-06 10:07:59 +0100

    Dodanie 3 buttonów (ustawienia daty przyjazdu, powrotu i obliczenia dlugosci przyjazdu) oraz pobranie daty z kalendarza po klknieciu odpowiednich przyciskow

--- app/src/main/java/com/example/calendarview/MainActivity.kt
+++ app/src/main/java/com/example/calendarview/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.calendarview
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.CalendarView
 import androidx.appcompat.app.AppCompatActivity
-import java.util.Calendar
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +14,25 @@ class MainActivity : AppCompatActivity() {
 
         val cal = CalendarView(this)
         cal.setDate(Calendar.getInstance().getTimeInMillis(),false,true);
+
+        var departure_date = findViewById<CalendarView>(R.id.kalendarz).date // DATA PRZYJAZDU
+        var date_of_return = findViewById<CalendarView>(R.id.kalendarz).date // DATA POWROTU
+
+        findViewById<Button>(R.id.btn_przyjazd).setOnClickListener {
+            departure_date = findViewById<CalendarView>(R.id.kalendarz).date
+        }
+
+        findViewById<Button>(R.id.btn_powrot).setOnClickListener {
+
+            if (departure_date <= date_of_return)
+            {
+
+            }
+
+            else
+            {
+
+            }
+        }
     }
 }
